53ab0f50a25d1741316b12ed52a39e4d7f7cca6b
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-04 21:54:05 -0400

    Add project files.

---
+++ Cost of balloons c#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());  // عدد حالات الاختبار
+
+        while (t-- > 0)
+        {
+            // قراءة تكلفة البالونات الخضراء والبنفسجية
+            string[] costs = Console.ReadLine().Split();
+            int greenCost = int.Parse(costs[0]);
+            int purpleCost = int.Parse(costs[1]);
+
+            // قراءة عدد المشاركين
+            int n = int.Parse(Console.ReadLine());
+
+            int firstScenarioCost = 0, secondScenarioCost = 0;
+
+            // حساب تكلفة السيناريوهات
+            for (int i = 0; i < n; i++)
+            {
+                string[] status = Console.ReadLine().Split();
+                int problem1 = int.Parse(status[0]);
+                int problem2 = int.Parse(status[1]);
+
+                // السيناريو الأول: المشكلة الأولى (بالون أخضر) والثانية (بالون بنفسجي)
+                firstScenarioCost += problem1 * greenCost + problem2 * purpleCost;
+
+                // السيناريو الثاني: المشكلة الأولى (بالون بنفسجي) والثانية (بالون أخضر)
+                secondScenarioCost += problem1 * purpleCost + problem2 * greenCost;
+            }
+
+            // طباعة أقل تكلفة
+            Console.WriteLine(Math.Min(firstScenarioCost, secondScenarioCost));
+        }
+    }
+}
+
